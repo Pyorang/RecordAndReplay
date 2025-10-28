fef3ce38e9958b88ee84e1df30b77f56ccf5005f
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-10-28 18:11:42 +0900

    플레이어 이동까지 구현 (18:30)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    public int moveSpeed = 1;
+    public int jumpForce = 1;
+    public Vector2 inputVec;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
+        rb.position += inputVec * moveSpeed * Time.deltaTime;
+    }
 
+    public void ActionMove(InputAction.CallbackContext context)
+    {
+        if(context.performed || context.canceled)
+        {
+            float input = context.ReadValue<float>();
+            inputVec = new Vector2(input, 0);
+        }
+    }
+
+    public void ActionJump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            rb.AddForce(new Vector2(0,1) * jumpForce);
+        }
     }
 }
